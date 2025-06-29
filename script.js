78c506b7d2e8679bf5aa9b9fd3b36536c7914635
Priyanshi <126442486+pikupika@users.noreply.github.com>
2025-06-29 15:40:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 console.log("Welcome to the Rock, Paper, Scissors game!");
 
-        const choices = ['rock', 'paper', 'scissors'];
+const choices = ['rock', 'paper', 'scissors'];
 
-        let human_choice = prompt("Enter your choice (rock, paper, or scissors) : ").toLowerCase();
+let human_choice = prompt("Enter your choice (rock, paper, or scissors) : ").toLowerCase();
 
-        let computer_choice = Math.floor(Math.random() * choices.length);
+let computer_choice = Math.floor(Math.random() * choices.length);
 
-        console.log("You chose: " + human_choice);
-        console.log("Computer chose: " + choices[computer_choice]);
+console.log("You chose: " + human_choice);
+console.log("Computer chose: " + choices[computer_choice]);
 
         
-        if (computer_choice === 'rock' && human_choice === 'scissors' || computer_choice === 'paper' && human_choice === 'rock' || computer_choice === 'scissors' && human_choice === 'paper') {
-            console.log("you lose!");
-        } else if ((computer_choice === 'rock' && human_choice === 'rock' || computer_choice === 'scissors' && human_choice === 'scissors' || computer_choice === 'paper' && human_choice === 'paper')) {
-           console.log("It's a tie");
-        } else {
-            console.log("You win!");
-        }
+if (computer_choice === 'rock' && human_choice === 'scissors' || 
+    computer_choice === 'paper' && human_choice === 'rock' || 
+    computer_choice === 'scissors' && human_choice === 'paper') {
+        console.log("you lose!");
+} else if (computer_choice === 'rock' && human_choice === 'rock' || 
+            computer_choice === 'scissors' && human_choice === 'scissors' || 
+            computer_choice === 'paper' && human_choice === 'paper') {
+        console.log("It's a tie");
+} else {
+        console.log("You win!");
+}
